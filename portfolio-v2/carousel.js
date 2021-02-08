719aad90c63569415b10672d56eefa660079c53f
Toni Powell <tonipow3ll@gmail.com>
2021-02-08 18:17:56 -0500

    moving things around

---
+++ portfolio-v2/carousel.js
@@ -0,0 +1,26 @@
+
+
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        autoHeight:true,
+        nav:true,
+        responsiveClass:true,
+        responsive:{
+          0:{
+              items:1,
+          },
+          600:{
+              items:1,
+          },
+          1000:{
+              items:1
+          }
+        }
+        
+      });  
+
+// end script
---
+++ portfolio-v2/script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+    $("#projects").on('click', function(){
+       $('#projectCards').removeClass("is-hidden");
+    })
+    $("#hideProjects").on('click', function(){
+        $('#projectCards').addClass('is-hidden');
+    })
+
+    // carousel
+   
+
+
+// end script
+});
