fe2cdf297666dd81b46e55c2ef976dd0e568c4b4
Toni Powell <tonipow3ll@gmail.com>
2021-02-09 10:57:13 -0500

    navbar works on mobile ish

--- portfolio-v2/script.js
+++ portfolio-v2/script.js
@@ -7,8 +7,15 @@ $( document ).ready(function() {
         $('#projectCards').addClass('is-hidden');
     })
 
-    // carousel
-   
+   //nav bar
+  // Get all "navbar-burger" elements
+  $(".navbar-burger").click(function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+});
 
 
 // end script
