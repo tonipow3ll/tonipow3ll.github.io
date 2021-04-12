fd32815aa9078851c18f6dab5ecc4eba00cb8c2a
Toni Powell <tonipow3ll@gmail.com>
2021-04-12 16:44:57 -0400

    adding gitignore

---
+++ portfolio-api/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sendGrid = require('@sendgrid/mail')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-AllowOrigin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Controlow-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get()
