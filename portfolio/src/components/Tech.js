3ba9b255160d2f8d2f3960261007daa3ee83848c
Toni Powell <tonipow3ll@gmail.com>
2021-04-13 14:40:50 -0400

    icons

--- portfolio/src/components/Tech.js
+++ portfolio/src/components/Tech.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import heroku from '../assets/images/heroku.jpg';
+import mongo from '../assets/images/mongo.jpg';
+import node from '../assets/images/node.jpg';
+import react from '../assets/images/react.jpg';
+import sequelize from '../assets/images/Sequelize.png';
+import git from '../assets/images/git.jpg';
 
 class Tech extends React.Component {
 
@@ -10,18 +15,14 @@ class Tech extends React.Component {
 
             <Container fluid={true}>
             <Row className="justify-content-around pr-5 pt-5">
+            <img className="icons" src={git} alt=""/>
+            <img className="icons" src={node} alt=""/>
+            <img className="icons" src={mongo} alt=""/>
+            <img className="icons" src={react} alt=""/>
+            <img className="icons" src={sequelize} alt=""/>
             <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-            <img className="icons" src={heroku} alt=""/>
-           <p>Tech Tech Tech</p>
-           <p>Tech Tech Tech</p>
+           {/* <p>Tech Tech Tech</p>
+           <p>Tech Tech Tech</p> */}
             </Row>
         </Container>
 
